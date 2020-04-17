6e95490f1f03d2241a1320392c5bd80e8c428992
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 12:47:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,11 @@
 def oxford_comma(array)
 if array.size == 1
   return array[0]
+elsif array.size == 2
+  array[1] = " and #{array[1]}"
+  return array.join
+  
+  
 end
 # final_element_replacement = "and #{array.last}"
 # array.pop
