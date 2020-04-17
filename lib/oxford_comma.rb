cda41e55c93565663ac0974908f7db50fd3d02d3
Chibuisi Enyia <enyia21@gmail.com>
2020-04-17 12:48:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,12 @@ if array.size == 1
 elsif array.size == 2
   array[1] = " and #{array[1]}"
   return array.join
-
-
+else
+  final_element_replacement = "and #{array.last}"
+  array.pop
+  array << final_element_replacement
+  return array.join(", ")  
 end
-# final_element_replacement = "and #{array.last}"
-# array.pop
-# array << final_element_replacement
-# array.join(", ")
+
 
 end
